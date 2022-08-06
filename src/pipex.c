d5fbe9829692a8f0b5d5d07ab50e24d1a52dec57
matcardo <matcardo@student.42sp.org.br>
2022-08-06 04:47:16 +0200

    how wait() works?

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: matcardo <matcardo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 05:03:53 by matcardo          #+#    #+#             */
-/*   Updated: 2022/07/26 22:45:19 by matcardo         ###   ########.fr       */
+/*   Updated: 2022/08/06 04:35:36 by matcardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,21 @@
 
 int	main()
 {
-	int	id;
+	int id = fork();
+	int	n;
 
-	id = fork();
-	printf("oii, from id %d\n", id);
-	if (!id)
-		printf("hellor from child process\n");
+	if (id == 0)
+		n = 1;
 	else
-		printf("hello from the main\n");
-	fct();
+		n = 6;
+	if (id != 0)	
+		wait(NULL);
+	int i = n;
+	for (i = n; i < n + 5; i++)
+	{
+		printf("%d ", i);
+		fflush(stdout);
+	}
+	printf("\n");
 	return (0);
 }
