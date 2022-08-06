0891763ca59996d2eb315fd603cdcd88fd343199
matcardo <matcardo@student.42sp.org.br>
2022-08-06 22:41:44 +0200

    how getpid and getppid works

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: matcardo <matcardo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 05:03:53 by matcardo          #+#    #+#             */
-/*   Updated: 2022/08/06 04:35:36 by matcardo         ###   ########.fr       */
+/*   Updated: 2022/08/06 22:27:48 by matcardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,11 @@
 int	main()
 {
 	int id = fork();
-	int	n;
-
-	if (id == 0)
-		n = 1;
-	else
-		n = 6;
-	if (id != 0)	
-		wait(NULL);
-	int i = n;
-	for (i = n; i < n + 5; i++)
-	{
-		printf("%d ", i);
-		fflush(stdout);
-	}
-	printf("\n");
+	int id2 = -1;
+	int id3 = -1;
+	id2 = fork();
+	id3 = fork();
+	
+	printf("id1: %d, id2: %d, id3: %d,Current (getpid): %d, parent (getppid): %d\n", id, id2, id3, getpid(), getppid());
 	return (0);
 }
