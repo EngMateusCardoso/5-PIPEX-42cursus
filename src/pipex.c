522aa0c141d69786e503c95cc063fe6ceb9a34cf
matcardo <matcardo@student.42sp.org.br>
2022-08-07 04:38:50 +0200

    second exercise for comunication between process, for 3 process

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: matcardo <matcardo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 05:03:53 by matcardo          #+#    #+#             */
-/*   Updated: 2022/08/07 04:20:45 by matcardo         ###   ########.fr       */
+/*   Updated: 2022/08/07 04:38:02 by matcardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int	main()
 	int arrSize = sizeof(arr) / sizeof(int);
 	int id, start, end;
 	int fd[2];
+	// int fd2[2];
 	// fd[0] - read
 	// fd[1] - wirte
 	if (pipe(fd))
@@ -25,15 +26,27 @@ int	main()
 	id = fork();
 	if (id == -1)
 		printf("Error in fork!");
+		
 	if (id == 0)
 	{
 		start = 0;
-		end = arrSize / 2;
+		end = arrSize / 3;
 	}
 	else
 	{
-		start = arrSize / 2;
-		end = arrSize;
+		id = fork();
+		if (id == -1)
+			printf("Error in fork 2!");
+		if (id == 0)
+		{
+			start = arrSize / 3;
+			end = arrSize / 3 * 2;
+		}
+		else
+		{
+			start = arrSize / 3 * 2;
+			end = arrSize;
+		}
 	}
 	int sum = 0;
 
@@ -55,11 +68,15 @@ int	main()
 		close(fd[1]);
 		if (read(fd[0], &sumFromChild, sizeof(int)) == -1)
 			printf("Erro on read!");
+		sum += sumFromChild;
+		if (read(fd[0], &sumFromChild, sizeof(int)) == -1)
+			printf("Erro on read!");
+		sum += sumFromChild;
 		close(fd[0]);
-		printf("final sum: %d\n", sum + sumFromChild);
+		printf("final sum: %d\n", sum);
 		wait(NULL);
 	}
-	printf("%d\n", sum);
+	// printf("%d\n", sum);
 
 	return (0);
 }
