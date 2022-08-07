1dd23d34537e7a36b77e567b7587a1e44b9da615
matcardo <matcardo@student.42sp.org.br>
2022-08-07 04:21:30 +0200

    exercise 1 with comunication between process

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: matcardo <matcardo@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/15 05:03:53 by matcardo          #+#    #+#             */
-/*   Updated: 2022/08/07 01:22:20 by matcardo         ###   ########.fr       */
+/*   Updated: 2022/08/07 04:20:45 by matcardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int	main()
 {
-	int id;
+	int arr[] = {1,2,3,4,5,6,7,8,9};
+	int arrSize = sizeof(arr) / sizeof(int);
+	int id, start, end;
 	int fd[2];
 	// fd[0] - read
 	// fd[1] - wirte
@@ -25,22 +27,39 @@ int	main()
 		printf("Error in fork!");
 	if (id == 0)
 	{
+		start = 0;
+		end = arrSize / 2;
+	}
+	else
+	{
+		start = arrSize / 2;
+		end = arrSize;
+	}
+	int sum = 0;
+
+  	for (int i = start; i < end; i++)
+	{
+		sum += arr[i];	
+	}
+	
+	if (!id)
+	{
 		close(fd[0]);
-		int x;
-		printf("Input a number: ");
-		scanf("%d", &x);
-		if (write(fd[1], &x, sizeof(int)) == -1)
+		if (write(fd[1], &sum, sizeof(int)) == -1)
 			printf("Erro on write!");
 		close(fd[1]);
 	}
 	else
 	{
+		int sumFromChild;
 		close(fd[1]);
-		int y;
-		if (read(fd[0], &y, sizeof(int)) == -1)
+		if (read(fd[0], &sumFromChild, sizeof(int)) == -1)
 			printf("Erro on read!");
 		close(fd[0]);
-		printf("Got from  child process %d\n", y);
+		printf("final sum: %d\n", sum + sumFromChild);
+		wait(NULL);
 	}
+	printf("%d\n", sum);
+
 	return (0);
 }
